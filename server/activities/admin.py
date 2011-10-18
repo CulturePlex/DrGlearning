2ca44d4752b2f705d7e3ffe3f747c4e9a5fa041d
Diego Muñoz Escalante <escalant3@gmail.com>
2011-10-18 17:51:43 -0400

    Added career and level controls to relate activities to courses

--- server/activities/admin.py
+++ server/activities/admin.py
@@ -2,11 +2,41 @@
 from django.contrib import admin
 
 from activities.models import (Relational, VisualMemory, GeospatialAreas,
-                               AfterBefore, HangmanPuzzle)
+                               AfterBefore, HangmanPuzzle, Level)
 
 
-admin.site.register(Relational)
-admin.site.register(VisualMemory)
-admin.site.register(GeospatialAreas)
-admin.site.register(AfterBefore)
-admin.site.register(HangmanPuzzle)
+class LevelInline(admin.TabularInline):
+    model = Level
+    extra = 1
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    inlines = (LevelInline,)
+    list_filter = ['career']
+
+
+class RelationalAdmin(ActivityAdmin):
+    pass
+
+
+class VisualMemoryAdmin(ActivityAdmin):
+    pass
+
+
+class GeospatialAreasAdmin(ActivityAdmin):
+    pass
+
+
+class AfterBeforeAdmin(ActivityAdmin):
+    pass
+
+
+class HangmanPuzzleAdmin(ActivityAdmin):
+    pass
+
+
+admin.site.register(Relational, RelationalAdmin)
+admin.site.register(VisualMemory, VisualMemoryAdmin)
+admin.site.register(GeospatialAreas, GeospatialAreasAdmin)
+admin.site.register(AfterBefore, AfterBeforeAdmin)
+admin.site.register(HangmanPuzzle, HangmanPuzzleAdmin)
