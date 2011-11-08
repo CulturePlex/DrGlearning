5f222f9f7d407ef12e6e94155be770aa38601b92
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2011-11-08 12:22:29 +0100

    updating install function

--- android/DrGlearning/assets/www/app/controller/Careers.js
+++ android/DrGlearning/assets/www/app/controller/Careers.js
@@ -120,7 +120,7 @@ Ext.define('DrGlearning.controller.Careers', {
 		this.selectedcareer=career;
 		if (career.data.installed == "false") 
 		{
-			Ext.Msg.confirm("Install Career?","Are you sure you want to install this career?",this.getController("DaoController").installCareer(career.data.id));
+			Ext.Msg.confirm("Install Career?","Are you sure you want to install this career?",function(answer){console.log(answer);});
 		}
 		else 
 		{
