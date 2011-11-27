d6f0d8fdae618128198d21aec60b2cba463e6bea
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2011-11-27 17:44:01 +0100

    Showing Polygon Area in Geospatial Ativity

--- android/DrGlearning/assets/www/app/controller/activities/GeospatialController.js
+++ android/DrGlearning/assets/www/app/controller/activities/GeospatialController.js
@@ -26,7 +26,23 @@ Ext.define('DrGlearning.controller.activities.GeospatialController', {
         view.on({
             show: function(){
                 google.maps.event.trigger(map, 'resize');
-                map.panTo(new google.maps.LatLng(50.71462, 12.496889));
+                
+				//var elotro={"type":"Feature", "properties":{}, "geometry":{"type":"Polygon", "coordinates":[[[20.390625, 13.0078125], [48.515625, 1.0546875], [33.75, 18.6328125], [9.84375, 28.4765625], [11.25, 1.7578125], [22.5, 12.3046875], [20.390625, 13.0078125]]]}, "crs":{"type":"name", "properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}}};
+				var jsonfromserver=eval("(" + activity.data.area + ')');
+				var googleOptions = {
+				    strokeColor: "#FFFF00",
+				    strokeWeight: 7,
+				    strokeOpacity: 0.75
+				};
+				//var multipunto1={"type":"Feature", "properties":{}, "geometry":{"type":"Point", "coordinates":[37.265625, -16.5234375]}, "crs":{"type":"name", "properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}}};
+				//var multipunto=eval("(" + activity.data.point + ')');
+				//var googlePuntos =new GeoJSON(multipunto, googleOptions);
+				//console.log(multipunto1);
+				//console.log(multipunto);
+				//googlePuntos.setMap(map);
+				googleVector = new GeoJSON(jsonfromserver, googleOptions);
+				googleVector.setMap(map);
+				map.panTo(new google.maps.LatLng(0.71462, 12.496889));
             }
         });
         // FIX: Rendering Problem von Sencha Touch 2.0.0-pr1
