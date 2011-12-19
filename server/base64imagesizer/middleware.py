806b62e400ba7d00cb5bc9414b1359f0ca10cfde
Diego Muñoz Escalante <escalant3@gmail.com>
2011-12-19 16:43:26 +0100

    Added Middleware files correctly

---
+++ server/base64imagesizer/middleware.py
@@ -0,0 +1,36 @@
+import base64
+
+from django.utils import simplejson
+from io import BytesIO
+from StringIO import StringIO
+from PIL import Image
+
+
+TARGET_FIELDS = (
+    ("/api/v1/activity/", ("image",)),
+)
+
+
+class Base64ImageSizer(object):
+
+    def process_response(self, request, response):
+        # Detect with re if petition is modificable
+        for tf in TARGET_FIELDS:
+            if request.path.startswith(tf[0]):
+                json = simplejson.loads(response.content)
+                for field in [f for f in json if f in tf[1]]:
+                    if 'deviceWidth' in request.GET and \
+                            'deviceHeight' in request.GET:
+                        maximum_size = min((int(request.GET['deviceWidth']),
+                                        int(request.GET['deviceHeight'])))
+                        size = maximum_size, maximum_size
+                        image_data = json[field].split('base64,')[1]
+                        image = BytesIO(base64.b64decode(image_data))
+                        im = Image.open(image)
+                        im.thumbnail(size)
+                        output = StringIO()
+                        im.save(output, format="PNG")
+                        json[field] = "data:image/png;base64,%s" % \
+                                        base64.encodestring(output.getvalue())
+                        response.content = simplejson.dumps(json)
+        return response
