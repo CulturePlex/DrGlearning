d4e7a069fbe75d645751b371168174124ca95cc0
Diego Muñoz Escalante <escalant3@gmail.com>
2012-03-02 19:50:28 -0500

    Added career size in characters

--- server/knowledges/api.py
+++ server/knowledges/api.py
@@ -22,8 +22,17 @@ class CareerResource(ModelResource):
         queryset = Career.objects.all()
 
     def dehydrate(self, bundle):
+        # Career creator name
         bundle.data["creator"] = bundle.obj.user.get_full_name() or \
                                                     bundle.obj.user.username
+
+        # Career size in bytes
+        size = 0
+        for activity in bundle.data["activities"]:
+            size += len(str(activity.data))
+        size += len(str(bundle.data))
+        bundle.data["size"] = size
+
         return bundle
 
     def alter_list_data_to_serialize(self, request, data):
@@ -33,4 +42,3 @@ class CareerResource(ModelResource):
                 if c.obj.activity_set.count() > 0]
         data["objects"] = filtered_careers
         return data
-
