aed3f6b2d9174c8dd07e67507f08c2e89f1614d1
Pedro Almagro <palmagroblanco@gmail.com>
2012-04-19 16:01:31 +0200

    Image invisual centered and style in home improved

--- server/base/static/js/index.js
+++ server/base/static/js/index.js
@@ -6,7 +6,12 @@
                   , ' poetry?'];
 
         var txt = $('#txtlzr');
-
+        var options = {
+            duration: 1000,          // Time (ms) each blurb will remain on screen
+            rearrangeDuration: 1000, // Time (ms) a character takes to reach its position
+            effect: 'random',        // Animation effect the characters use to appear
+            centered: true           // Centers the text relative to its container
+          }
         txt.textualizer(list, { duration: 1000 });
         txt.textualizer('start');
     });
