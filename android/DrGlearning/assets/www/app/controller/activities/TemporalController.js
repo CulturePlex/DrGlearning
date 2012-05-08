3af93159f5fea351c82bd8c79721af859159ef00
Pedro Almagro <palmagroblanco@gmail.com>
2012-05-08 05:14:41 +0200

    Temporal Controller Code Beautified

--- android/DrGlearning/assets/www/app/controller/activities/TemporalController.js
+++ android/DrGlearning/assets/www/app/controller/activities/TemporalController.js
@@ -3,21 +3,16 @@
 
 Ext.define('DrGlearning.controller.activities.TemporalController', {
     extend: 'Ext.app.Controller',
-    requires: ['DrGlearning.store.Careers', 'DrGlearning.store.Levels', 'DrGlearning.view.CareersFrame'],
-    views: ['ActivityFrame', 'activities.Temporal'],
-    controllers: ['DrGlearning.controller.Careers'],
-    stores: ['Careers', 'Levels', 'Activities'],
-    refs: [{
-        ref: 'activities.temporal',
-        selector: 'mainview',
-        autoCreate: true,
-        xtype: 'mainview'
-    }],
+
     activity: null,
-    puntos: null,
+    score: null,
+
     init: function ()
-	{
+    {
         this.levelController = this.getApplication().getController('LevelController');
+        this.activityController = this.getApplication().getController('ActivityController');
+        this.daoController = this.getApplication().getController('DaoController');
+
         this.control({
             'button[customId=after]': {
                 tap: this.after
@@ -28,7 +23,7 @@ Ext.define('DrGlearning.controller.activities.TemporalController', {
         });
     },
     updateActivity: function (view, newActivity)
-	{
+    {
         Ext.Viewport.setMasked({
             xtype: 'loadmask',
             message: i18n.gettext('Loading activity...'),
@@ -40,53 +35,53 @@ Ext.define('DrGlearning.controller.activities.TemporalController', {
             view.down('component[customId=activity]').hide();
             view.down('component[customId=activity]').destroy();
         }
-        activityView = Ext.create('DrGlearning.view.activities.Temporal');
-        this.getApplication().getController('ActivityController').addQueryAndButtons(activityView, newActivity);
+        var activityView = Ext.create('DrGlearning.view.activities.Temporal');
+        this.activityController.addQueryAndButtons(activityView, newActivity);
         newActivity.getImage('image', 'image', activityView.down('[id=image]'), this, view, activityView, false);
     },
     loadingImages: function (view, activityView)
-	{
+    {
         activityView.show();
         view.add(activityView);
         Ext.Viewport.setMasked(false);
         if (!this.activity.data.help) {
             this.activity.data.help = true;
             this.activity.save();
-            this.getApplication().getController('LevelController').helpAndQuery();
+            this.levelController.helpAndQuery();
         }
     },
     before: function ()
-	{
-        this.puntos = 100;
+    {
+        this.score = 100;
         if (this.activity.data.image_datetime < this.activity.data.query_datetime) {
-            Ext.Msg.alert(i18n.gettext('Success!'), this.activity.data.reward + ' ' + i18n.gettext("obtained score:") + this.puntos, function ()
-			{
-                this.getApplication().getController('DaoController').activityPlayed(this.activity.data.id, true, this.puntos);
-                this.getApplication().getController('LevelController').nextActivity(this.activity.data.level_type);
+            Ext.Msg.alert(i18n.gettext('Success!'), this.activity.data.reward + ' ' + i18n.gettext("obtained score:") + this.score, function ()
+            {
+                this.daoController.activityPlayed(this.activity.data.id, true, this.score);
+                this.levelController.nextActivity(this.activity.data.level_type);
             }, this);
         }
         else 
-		{
+        {
             Ext.Msg.alert(i18n.gettext('Wrong!'), i18n.gettext('Oooh, it wasnt the correct answer'), function ()
-			{
-                this.getApplication().getController('LevelController').tolevel();
+            {
+                this.levelController.tolevel();
             }, this);
         }
     },
     after: function ()
-	{
-        this.puntos = 100;
+    {
+        this.score = 100;
         if (this.activity.data.image_datetime > this.activity.data.query_datetime) {
-            Ext.Msg.alert(i18n.gettext('Success!'), this.activity.data.reward + ' ' + i18n.gettext("obtained score:") + this.puntos, function ()
-			{
-                this.getApplication().getController('DaoController').activityPlayed(this.activity.data.id, true, this.puntos);
-                this.getApplication().getController('LevelController').nextActivity(this.activity.data.level_type);
+            Ext.Msg.alert(i18n.gettext('Success!'), this.activity.data.reward + ' ' + i18n.gettext("obtained score:") + this.score, function ()
+            {
+                this.daoController.activityPlayed(this.activity.data.id, true, this.score);
+                this.levelController.nextActivity(this.activity.data.level_type);
             }, this);
         }
         else {
             Ext.Msg.alert(i18n.gettext('Wrong!'), this.activity.data.penalty, function ()
-			{
-                this.getApplication().getController('LevelController').tolevel();
+            {
+                this.levelController.tolevel();
             }, this);
         }
     }
