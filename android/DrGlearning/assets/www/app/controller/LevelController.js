eb56ce4a942b8289b762487cda648c302ca7a02e
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-05-14 23:49:47 +0200

    Level Controller code beutified and JSHint validated

--- android/DrGlearning/assets/www/app/controller/LevelController.js
+++ android/DrGlearning/assets/www/app/controller/LevelController.js
@@ -12,8 +12,6 @@ Ext.define('DrGlearning.controller.LevelController', {
             activityframe: 'activityframe'
         }
     },
-    activityView: null,
-    carousel: null,
     currentActivity: null,
     /*
      * Initializate Controller.
@@ -22,15 +20,31 @@ Ext.define('DrGlearning.controller.LevelController', {
         this.careersListController = this.getApplication().getController('CareersListController');
         this.careerController = this.getApplication().getController('CareerController');
         this.levelController = this.getApplication().getController('LevelController');
+        this.daoController = this.getApplication().getController('DaoController');
+        
+        this.quizController = this.getApplication().getController('activities.QuizController');
+        this.visualController = this.getApplication().getController('activities.VisualController');
+        this.relationalController = this.getApplication().getController('activities.RelationalController');
+        this.geospatialController = this.getApplication().getController('activities.GeospatialController');
+        this.linguisticController = this.getApplication().getController('activities.LinguisticController');
+        this.temporalController = this.getApplication().getController('activities.TemporalController');
+        
+        this.activitiesStore = Ext.getStore('Activities');
+        this.levelsStore = Ext.getStore('Levels');
         
         Ext.create('DrGlearning.view.ActivityFrame');
         Ext.create('DrGlearning.view.LevelFrame');
+        
+        this.detail = this.getLevelframe().down('leveldetail');
+        this.activitieslist= this.getLevelframe().down('list[customId=activitiesList]');
+        this.levelTitle = this.getLevelframe().down('title[id=title]');
+        this.activityTitle = this.getActivityframe().down('title[customId=title]');
         
         this.control({
             'button[id=help]': {
                 tap: this.help
             },
-			 '[customId=query_label]': {
+             '[customId=query_label]': {
                 click: this.more
             },
             'button[id=backtolevels]': {
@@ -45,17 +59,23 @@ Ext.define('DrGlearning.controller.LevelController', {
             '[customId=centro]': {
                 tap: this.startActivity
             },
-			'list[customId=activitiesList]': {
+            'list[customId=activitiesList]': {
                 itemtap: this.startActivity
             }
         });
     },
+    /*
+     * Showing levels view.
+     */
     tolevels: function(){
         this.getLevelframe().hide();
         this.careerController.tocareer();
     },
+     /*
+     * Updating and Showing Level view.
+     */
     updateLevel: function(newCareer, newLevel){
-		Ext.Viewport.setMasked({
+        Ext.Viewport.setMasked({
             xtype: 'loadmask',
             message: i18n.gettext('Loading level...'),
             indicator: true
@@ -63,111 +83,93 @@ Ext.define('DrGlearning.controller.LevelController', {
         });
 
         this.selectedlevel = newLevel;
-        var view = this.getLevelframe();
-		view.setListeners( {
+        this.getLevelframe().setListeners( {
                 painted: function(){
                     Ext.Viewport.setMasked(false);
                 }
             });
-        var detail = view.down('leveldetail');
-		var activitieslist=detail.down('list[customId=activitiesList]');
-		var filesImgs=["iletratumB.png","primaryB.png","secondaryB.png","highschoolB.png","collegeB.png","masterB.png","PhDB.png","post-docB.png","professorB.png","emeritusB.png"];
-		activitieslist.setStyle( {
-			            backgroundImage: 'url(resources/images/level_icons/'+filesImgs[newLevel-1]+')',
-			            backgroundRepeat: 'no-repeat',
-			            backgroundPosition: 'center',
-						backgroundFilter: 'alpha(opacity=60)'
-			});
-		activitieslist.refresh();
-        var level = Ext.getStore('Levels').getAt(newLevel - 1);
-		Ext.getStore('Activities').sort('level_order');
-		//Ext.getStore('Activities').sort('successful');
-		Ext.getStore('Activities').clearFilter();
-		Ext.getStore('Activities').filter('careerId',newCareer.data.id);
-		Ext.getStore('Activities').filter({filterFn: function(item) { return item.data.level_type == newLevel; }});
-		console.log(Ext.getStore('Activities'));
-        view.down('title[id=title]').setTitle(newCareer.data.name);
-		
-		
-        view.show();
+        var filesImgs=["iletratumB.png","primaryB.png","secondaryB.png","highschoolB.png","collegeB.png","masterB.png","PhDB.png","post-docB.png","professorB.png","emeritusB.png"];
+        this.activitieslist.setStyle( {
+                        backgroundImage: 'url(resources/images/level_icons/'+filesImgs[newLevel-1]+')',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                        backgroundFilter: 'alpha(opacity=60)'
+            });
+        this.activitieslist.refresh();
+        var level = this.levelsStore.getAt(newLevel - 1);
+        this.activitiesStore.sort('level_order');
+        this.activitiesStore.clearFilter();
+        this.activitiesStore.filter('careerId',newCareer.data.id);
+        this.activitiesStore.filter({filterFn: function(item) { return item.data.level_type == newLevel; }});
+        this.levelTitle.setTitle(newCareer.data.name);
+        this.getLevelframe().show();
         if (typeof(MathJax) !== "undefined") {
             MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
         }
     },
-    
-    getActivityHtml: function(activityData){
-        var html = "<div id='centro' align='middle'><p align='top'>" + activityData.name + "</p><a href= 'javascript:this.levelController.startActivity();'><img src='resources/images/activity_icons/" + activityData.activity_type + ".png' align='bottom'></a></div>";
-        console.log(activityData.successful);
-        if (activityData.successful == true) {
-            html = "<div id='centro' align='middle'><p align='top'>" + activityData.name + "</p><a href= 'javascript:this.levelController.startActivity();'><img src='resources/images/activity_icons/" + activityData.activity_type + ".png' align='bottom'></a></div><div bottom='0' align='center'>Score: " + activityData.score + "<img  height='30px' src=resources/images/tick.png></div>";
-        }
-        return html;
-        
-    },
-	startActivity: function(list, itemIndex, item, activity,e){
+    /*
+     * Starting activity.
+     */
+    startActivity: function(list, itemIndex, item, activity,e){
         this.updateActivity(activity);
         if (this.getLevelframe()) {
             this.getLevelframe().hide();
         }
     
     },
-    
+     /*
+     * Updating activity view.
+     */
     updateActivity: function(newActivity){
-        console.log(this);
         this.currentActivity = newActivity;
-        Ext.create('DrGlearning.view.ActivityFrame');
-        var view = this.getActivityframe();
-        Ext.ComponentQuery.query('title[customId=title]')[0].setTitle(newActivity.data.name);
-        var activityView;
+        this.activityTitle.setTitle(newActivity.data.name);
         if (newActivity.data.activity_type == 'geospatial') {
            /* if (navigator.network == undefined || navigator.network.connection.type == Connection.NONE) {
              Ext.Msg.alert(i18n.gettext('No Internet'), i18n.gettext('There is not connection to Internet, you cant start this activity!'), function(){
              this.tolevel();
              }, this);
              }else{*/
-            	this.getApplication().getController('activities.GeospatialController').updateActivity(view, newActivity);
+                this.geospatialController.updateActivity(this.getActivityframe(), newActivity);
             //}
         }
         else 
             if (newActivity.data.activity_type == 'visual') {
-                this.getApplication().getController('activities.VisualController').updateActivity(view, newActivity);
+                this.visualController.updateActivity(this.getActivityframe(), newActivity);
             }
             else 
                 if (newActivity.data.activity_type == 'relational') {
                     console.log(newActivity.data);
-                    this.getApplication().getController('activities.RelationalController').updateActivity(view, newActivity);
+                    this.relationalController.updateActivity(this.getActivityframe(), newActivity);
                 }
                 else 
                     if (newActivity.data.activity_type == 'temporal') {
-                        this.getApplication().getController('activities.TemporalController').updateActivity(view, newActivity);
+                        this.temporalController.updateActivity(this.getActivityframe(), newActivity);
                     }
                     else 
                         if (newActivity.data.activity_type == 'linguistic') {
-                            this.getApplication().getController('activities.LinguisticController').updateActivity(view, newActivity);
+                            this.linguisticController.updateActivity(this.getActivityframe(), newActivity);
                         }
                         else 
                             if (newActivity.data.activity_type == 'quiz') {
-                                this.getApplication().getController('activities.QuizController').updateActivity(view, newActivity);
+                                this.quizController.updateActivity(this.getActivityframe(), newActivity);
                             }
-        view.show();
+        this.getActivityframe().show();
     },
+     /*
+     * Updating and Showing next activity when you success one.
+     */
     nextActivity: function(prevLevel){
-        if (this.activityView) {
-            this.activityView.hide();
+        var currentLevel = this.daoController.getCurrenLevel(this.careersListController.selectedcareer.data.id);
+        var prevLevelString = this.levelsStore.getAt(prevLevel - 1).data.name;
+        var currentLevelString;
+        if (currentLevel != -1) {
+            currentLevelString = this.levelsStore.getAt(currentLevel - 1).data.name;
+        }else
+        {
+                currentLevelString = 'Error';
         }
-        
-        var currentLevel = this.getApplication().getController('DaoController').getCurrenLevel(this.careersListController.selectedcareer.data.id);
-        var prevLevelString = Ext.getStore('Levels').getAt(prevLevel - 1).data.name;
-		if (currentLevel != -1) {
-			var currentLevelString = Ext.getStore('Levels').getAt(currentLevel - 1).data.name;
-		}else
-		{
-				var currentLevelString = 'Error';
-		}
-        console.log(currentLevel);
-        console.log(prevLevel);
-        var currentActivity = this.getApplication().getController('DaoController').getCurrenActivity(this.careersListController.selectedcareer.data.id, parseInt(prevLevel));
-        if (currentActivity.data.successful == false) {
+        var currentActivity = this.daoController.getCurrenActivity(this.careersListController.selectedcareer.data.id, parseInt(prevLevel,10));
+        if (currentActivity.data.successful === false) {
             this.updateActivity(currentActivity);
         }
         else {
@@ -177,14 +179,14 @@ Ext.define('DrGlearning.controller.LevelController', {
                 }
                 this.careersListController.index();
                 this.getActivityframe().hide();
-                Ext.Msg.alert(i18n.gettext('Congrats!'), i18n.gettext('You have complete the ') + prevLevelString + i18n.gettext(' level! It was the last Level, you have finished this career!'), function(){}, this);;
+                Ext.Msg.alert(i18n.gettext('Congrats!'), i18n.gettext('You have complete the ') + prevLevelString + i18n.gettext(' level! It was the last Level, you have finished this career!'), function(){}, this);
             }
             else {
                 this.careerController.updateCareer(this.careersListController.selectedcareer);
                 this.getLevelframe().hide();
                 this.getActivityframe().hide();
                 if (currentLevel != -1) {
-					
+                    
                     Ext.Msg.alert(i18n.gettext('Congrats!'), i18n.gettext('You have complete the ') + prevLevelString +  i18n.gettext(' level! Next Level: ') + currentLevelString , function(){}, this);
                 }
                 else {
@@ -194,8 +196,11 @@ Ext.define('DrGlearning.controller.LevelController', {
         }
     
     },
+    /*
+     * Back to level view.
+     */
     tolevel: function(){
-		Ext.Viewport.setMasked({
+        Ext.Viewport.setMasked({
             xtype: 'loadmask',
             message: 'Loading level...',
             indicator: true
@@ -204,13 +209,14 @@ Ext.define('DrGlearning.controller.LevelController', {
         if (this.getActivityframe()) {
             this.getActivityframe().hide();
         }
-        this.getApplication().getController('activities.VisualController').stop();
-        this.getApplication().getController('activities.QuizController').stop();
-        var view1 = this.getLevelframe();
+        this.visualController.stop();
+        this.quizController.stop();
         this.updateLevel(this.careersListController.selectedcareer, this.levelController.selectedlevel);
-		
-        view1.show();
+        this.getLevelframe().show();
     },
+    /*
+     * Showing help in an activity.
+     */
     help: function(){
         var text = "help!";
         if (this.currentActivity.data.activity_type == 'linguistic') {
@@ -240,29 +246,31 @@ Ext.define('DrGlearning.controller.LevelController', {
         }
         if (this.currentActivity.data.activity_type == 'visual') {
             text = i18n.gettext("Look at the image and answer the question!");
-            this.getApplication().getController('activities.VisualController').stopNotClear();
+            this.visualController.stopNotClear();
             Ext.Msg.alert(i18n.gettext('Help'), text, function(){
-                this.getApplication().getController('activities.VisualController').restart()
+                this.visualController.restart();
             }, this);
         }
     },
+    /*
+     * Showing the complete query of an activity.
+     */
     more: function(that){
-        console.log(that);
         var text = that.currentActivity.data.query;
         Ext.Msg.alert(i18n.gettext('Question'), text, function(){
         }, that);
         
     },
-	helpAndQuery: function(){
-		var text = this.currentActivity.data.query;
+    helpAndQuery: function(){
+        var text = this.currentActivity.data.query;
         if (this.currentActivity.data.activity_type == 'linguistic') {
-			text += " <br>  <br>";
-			text += i18n.gettext("You should to guess a sentence with help of the image and the tip text, you can unlock letters in the hide tip and parts of the image");
+            text += " <br>  <br>";
+            text += i18n.gettext("You should to guess a sentence with help of the image and the tip text, you can unlock letters in the hide tip and parts of the image");
             Ext.Msg.alert('Question and Help', text, function(){
             }, this);
         }
         if (this.currentActivity.data.activity_type == 'geospatial') {
-			text += " <br>  <br>";
+            text += " <br>  <br>";
             text += i18n.gettext("You should find the correct location in the map");
             Ext.Msg.alert('Question and Help', text, function(){
             }, this);
@@ -288,11 +296,11 @@ Ext.define('DrGlearning.controller.LevelController', {
         if (this.currentActivity.data.activity_type == 'visual') {
             text += " <br>  <br>";
             text += i18n.gettext("Look at the image and answer the question!");
-            this.getApplication().getController('activities.VisualController').stopNotClear();
+            this.visualController.stopNotClear();
             Ext.Msg.alert(i18n.gettext('Question and Help'), text, function(){
-                this.getApplication().getController('activities.VisualController').restart()
+                this.visualController.restart();
             }, this);
         }
-		
-	}
+        
+    }
 });
