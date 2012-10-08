f6421e15d2020bcd077d006b1a5d2998173f897d
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-10-08 16:11:34 -0500

    Showing types in FreeBase search and JSHint in GraphExtractor success

--- server/base/static/js/GameMining/GraphExtractor.js
+++ server/base/static/js/GameMining/GraphExtractor.js
@@ -1,57 +1,67 @@
+/*jshint
+    forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:false,
+    undef:true, curly:true, browser:true, indent:2, maxerr:50 noempty:false
+*/
+
+/*global
+    jQuery $ GraphEditor
+*/
+
+
 /*if (typeof($) === "undefined") {
   $ = django.jQuery;
 }*/
 var GraphExtractor = {
   //Freebase Extractor Variables:
   jsonData : "",
-  search: function(name){
-    jQuery.get( "https://www.googleapis.com/freebase/v1/mqlread",'query= [{"name":"'+ $('#query')[0].value+'","type":[]}]' , function(data) {
-    GraphExtractor.showTypes(data.result);
-  });
+  search: function (name) {
+    jQuery.get("https://www.googleapis.com/freebase/v1/mqlread", 'query= [{"name":"' + $('#query')[0].value + '","type":[]}]', function (data) {
+      GraphExtractor.showTypes(data.result);
+    });
   },
-  get: function(name){
-    jQuery.get( "https://www.googleapis.com/freebase/v1/mqlread",'query={"name":"'+ $('#query')[0].value+'","type":"/music/artist","*":null}' , function(data) {
-    GraphExtractor.jsonData = data;
-    GraphExtractor.setGraph(data.result);
-  });
+  get: function (name) {
+    jQuery.get("https://www.googleapis.com/freebase/v1/mqlread", 'query={"name":"' + $('#query')[0].value + '","type":"/music/artist","*":null}', function (data) {
+      GraphExtractor.jsonData = data;
+      GraphExtractor.setGraph(data.result);
+    });
   },
-  init: function(){
+  init: function () {
     GraphEditor.progressBar.show();
     // Clean previous information to have a clean graph
     GraphEditor.setGraphNodesJSON({});
     GraphEditor.setGraphEdgesJSON([]);
     GraphEditor.setConstraints([]);
   },
-  showTypes: function(result){
-    $.each(result, function(k,v){
-      if(v.type instanceof Array)
+  showTypes: function (result) {
+    $.each(result, function (k, v) {
+      if (v.type instanceof Array)
       {
-        for(obj in v.type)
+        for (var obj in v.type)
         {
-          console.log(v.type[obj]);
+          $("#type_select_from").append('<option value=' + v.type[obj] + '>' + v.type[obj] + '</option>');
         }
       }
     });
   },
-  setGraph: function(result){
+  setGraph: function (result) {
 
-    GraphEditor.addNode(result.name,{type:"Banda",score:0},"FREEBASEGRAPH");
-    $.each(result, function(k,v){
+    GraphEditor.addNode(result.name, {type: "Banda", score: 0}, "FREEBASEGRAPH");
+    $.each(result, function (k, v) {
 
-      if(k != "name")
+      if (k !== "name")
       {
-        if(v instanceof Array)
+        if (v instanceof Array)
         {
 
-          for(obj in v)
+          for (var obj in v)
           {
-            GraphEditor.addNode(v[obj],{type:k,score:0});
+            GraphEditor.addNode(v[obj], {type:k, score: 0});
           }
-        }else
+        } else
         {
-          if(v != null)
+          if (v != null)
           {
-            GraphEditor.addNode(v,{type:k ,score:0},"FREEBASEGRAPH");
+            GraphEditor.addNode(v, {type: k, score: 0}, "FREEBASEGRAPH");
           }
         }
         
@@ -59,17 +69,17 @@ var GraphExtractor = {
       
     });
 
-    $.each(result, function(k,v){
+    $.each(result, function (k, v) {
 
-      if(v instanceof Array)
+      if (v instanceof Array)
       {
-        for(obj in v)
+        for (var obj in v)
         {
           GraphEditor.addEdge(result.name, k, v[obj], {inverse: "inverse of" + k});
         }
-      }else
+      } else
       {
-        if(v != null && k !=null)
+        if (v != null && k != null)
         {
           GraphEditor.addEdge(result.name, k, v, {inverse: "inverse of" + k});
         }
@@ -80,5 +90,5 @@ var GraphExtractor = {
     //GraphEditor.progressBar.hide();
     
   }
-}
+};
 
