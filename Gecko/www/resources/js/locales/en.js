213938c4ef286615aa1cb614ad469ad49c298585
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-10-17 12:14:08 -0500

    Restructuring folders

---
+++ Gecko/www/resources/js/locales/en.js
@@ -0,0 +1,6 @@
+// Locale en
+var i18n = new Jed({
+    "locale_data": {"messages": {"": {"Content-Transfer-Encoding": "8bit", "Content-Type": "text/plain; charset=UTF-8", "Language": "en", "Language-Team": "English (http://www.transifex.net/projects/p/drglearning/language/en/)", "Last-Translator": "versae <versae@gmail.com>", "MIME-Version": "1.0", "PO-Revision-Date": "2012-06-11 19:21+0000", "POT-Creation-Date": "2012-06-11 14:25-0400", "Plural-Forms": "nplurals=2; plural=(n != 1)", "Project-Id-Version": "Dr. Glearning", "Report-Msgid-Bugs-To": "", "lang": "en"}, "%d second": ["%d seconds", "", ""]}},
+    "domain": "messages",
+    "plural_forms": "nplurals=2; plural=(n != 1);"
+});
