4c1f99c880399fa1f7e5a3b4115db6f171559f06
Javier de la Rosa <versae@gmail.com>
2012-12-10 11:32:59 -0500

    Adding CORS support for django-tastypie

--- server/settings.py
+++ server/settings.py
@@ -127,6 +127,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'base.middleware.XsSharing',
     # 'activities.middleware.Base64ImageSizer',
 )
 
