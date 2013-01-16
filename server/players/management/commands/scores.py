487470cc23a49a22b654b6b8d80324aca51e0c22
Javier de la Rosa <versae@gmail.com>
2013-01-16 15:02:18 -0500

    Fix the formula for average when calculating scores report

--- server/players/management/commands/scores.py
+++ server/players/management/commands/scores.py
@@ -17,16 +17,16 @@ class Command(BaseCommand):
             raise CommandError("All the parameters must be provided.")
         career_id = args[0]
         career = Career.objects.get(id=career_id)
-        self.stdout.write("=" * len(career.name))
+        self.stdout.write("=" * len(career.name) + "\n")
         self.stdout.write(career.name)
-        self.stdout.write("=" * len(career.name))
+        self.stdout.write("\n" + "=" * len(career.name) + "\n")
         activity_count = career.activity_set.count() * 100
         params = {
             "highscore__activity__career__id": career_id,
         }
         players = Player.objects.filter(**params).distinct()
         for player in players:
-            self.stdout.write(player.get_name())
+            self.stdout.write(player.get_name() + u"\n")
             count = 0
             for activity in career.activity_set.all():
                 scores = HighScore.objects.filter(activity=activity,
@@ -34,12 +34,12 @@ class Command(BaseCommand):
                 max_score = scores.aggregate(max=Max("score")).get("max", 0.0)
                 if not max_score:
                     max_score = 0.0
-                self.stdout.write(u"\t{0}:\t{1}".format(activity.name,
+                self.stdout.write(u"\t{0}:\t{1}\n".format(activity.name,
                                                         max_score))
                 count += max_score
-            avg = 100 * activity_count / count
-            message = u"\tAVERAGE:\t{0} ({1}/{2})".format(avg, count,
+            avg = avg = 100 * (count / activity_count)
+            message = u"\tAVERAGE:\t{0} ({1}/{2})\n".format(avg, count,
                                                           activity_count)
-            self.stdout.write(u"\t--------")
-            self.stdout.write(message)
             self.stdout.write(u"\t--------\n")
+            self.stdout.write(message)
+            self.stdout.write(u"\n\t--------\n\n")
