33b0a1fbd6b5e053b105e96a12f1513833177921
Javier de la Rosa <versae@gmail.com>
2013-04-03 11:46:28 -0400

    Add option on the scores command to pass an player email

--- server/players/management/commands/scores.py
+++ server/players/management/commands/scores.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Max
 
@@ -7,7 +9,15 @@ from players.models import Player, HighScore
 
 
 class Command(BaseCommand):
-
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--email',
+            action='store',
+            dest='email',
+            type="string",
+            help='Set a specific player email to get the scores from'
+        ),
+    )
     args = "career_id"
     help = "\tShow a report witt scores for the career `career_id`."
     can_import_settings = True
@@ -24,9 +34,14 @@ class Command(BaseCommand):
         params = {
             "highscore__activity__career__id": career_id,
         }
+        email = options['email']
+        if email:
+            params.update({
+                "email__icontains": email,
+            })
         players = Player.objects.filter(**params).distinct()
         for player in players:
-            self.stdout.write(player.get_name() + u":\n")
+            self.stdout.write(player.get_name() + u":\n ")
             count = 0
             for activity in career.activity_set.all():
                 scores = HighScore.objects.filter(activity=activity,
@@ -35,11 +50,11 @@ class Command(BaseCommand):
                 if not max_score:
                     max_score = 0.0
                 self.stdout.write(u"\t{0}:\t{1}\n".format(activity.name,
-                                                        max_score))
+                                                          max_score))
                 count += max_score
             avg = avg = 100 * (count / activity_count)
             message = u"\tAVERAGE:\t{0} ({1}/{2})\n".format(avg, count,
-                                                          activity_count)
+                                                            activity_count)
             self.stdout.write(u"\t--------\n")
             self.stdout.write(message)
             self.stdout.write(u"\t--------\n\n")
