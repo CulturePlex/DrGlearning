d6409393efa3141eec3ea8ec49cd383f2c8b20ba
pedro <pedro@pedro-SATELLITE-L735.(none)>
2013-06-04 13:27:20 +0200

    markers moving according image movement in Click Image Activity

--- gecko/www/app/js/activities/ClickImage.js
+++ gecko/www/app/js/activities/ClickImage.js
@@ -2,14 +2,38 @@ var ClickImage = {
     activity: null,
     viewer: null,
     pointer: null,
+    coords: null,
+    timer: null,
+    is_dragging: null,
     setup: function(){
-        console.log('asd');
+        ClickImage.is_dragging = false;     
+  
         ClickImage.viewer = $("#clickImageImage").iviewer({
             src:"http://i618.photobucket.com/albums/tt262/royallaser/FrankZappaWereOnlyInItForTheMoneyIn.jpg", 
             zoom_base:100,
             onClick: function(ev, coords) {
-               ClickImage.setMarker(coords)
+               console.log(ClickImage.is_dragging);
+               if (!ClickImage.is_dragging)
+                   ClickImage.setMarker({x: coords.x, y: coords.y});
             },
+            onAfterZoom: function(ev, new_zoom){
+               if(ClickImage.coords != null)
+               ClickImage.replaceMarker(ClickImage.pointer,ClickImage.coords);
+            },
+            onDrag: function (ev, point){
+               if(ClickImage.coords != null)
+               ClickImage.replaceMarker(ClickImage.pointer,ClickImage.coords);
+            },
+            onStartDrag: function (ev, point){
+                ClickImage.is_dragging = true;
+                console.log("start Drag");
+                //ClickImage.timer = setTimeout(function(){ClickImage.is_dragging = true},500);
+            },
+            onStopDrag: function (ev, point){
+                setTimeout(function(){ClickImage.is_dragging = false;},100);
+                //window.clearTimeout(ClickImage.timer);
+                console.log("stop Drag");
+            }
         });
         $("#in").click(function(){ viewer.iviewer('zoom_by', 1); }); 
         $("#out").click(function(){ viewer.iviewer('zoom_by', -1); }); 
@@ -21,22 +45,39 @@ var ClickImage = {
         $('#clickImage').live( 'pageshow',function(event){
            ClickImage.viewer.iviewer('update');
         });
-
+        ClickImage.pointer = $("<div id='pointer'></div>");
+        $('.wrapper').append(ClickImage.pointer);
     },
     refresh: function(){
     
     },
     setMarker: function(coords){
-        ClickImage.pointer = $("<div id='pointer'></div>");
-        $('.wrapper').append(ClickImage.pointer);
+
+
         console.log(coords);
+        ClickImage.coords = coords;
         var offset = ClickImage.viewer.iviewer('imageToContainer', coords.x, coords.y);
         var containerOffset = ClickImage.viewer.iviewer('getContainerOffset');
         
         offset.x += containerOffset.left - 20;
         offset.y += containerOffset.top - 40;
         ClickImage.pointer.css('display', 'block');
-        ClickImage.pointer.css('left', offset.x+'px');
-        ClickImage.pointer.css('top', offset.y+'px');
+        ClickImage.pointer.css('left', (offset.x - 9) +'px');
+        ClickImage.pointer.css('top', (offset.y - 22) +'px');
+    },
+    replaceMarker: function(marker,coords){
+        console.log(marker);
+        console.log(coords);
+        var offset = ClickImage.viewer.iviewer('imageToContainer', coords.x, coords.y);
+        var containerOffset = ClickImage.viewer.iviewer('getContainerOffset');
+        
+        offset.x += containerOffset.left - 20;
+        offset.y += containerOffset.top - 40;
+        marker.css('display', 'block');
+        marker.css('left', (offset.x - 9)+'px');
+        marker.css('top', (offset.y - 22)+'px');
     }
+
+
+
 };
