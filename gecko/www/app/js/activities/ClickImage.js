4b41b988f30ed948837a5dbed862900af6a0e81e
pedro <pedro@pedro-SATELLITE-L735.(none)>
2013-06-04 12:13:40 +0200

    Click Image activity allowing put markers in image

--- gecko/www/app/js/activities/ClickImage.js
+++ gecko/www/app/js/activities/ClickImage.js
@@ -1,6 +1,7 @@
 var ClickImage = {
     activity: null,
     viewer: null,
+    pointer: null,
     setup: function(){
         console.log('asd');
         ClickImage.viewer = $("#clickImageImage").iviewer({
@@ -17,18 +18,25 @@ var ClickImage = {
         $("#update").click(function(){ viewer.iviewer('update_container_info'); });
         $(".iviewer_rotate_right").hide();
         $(".iviewer_rotate_left").hide();
-        $( '#clickImage' ).live( 'pageshow',function(event){
+        $('#clickImage').live( 'pageshow',function(event){
            ClickImage.viewer.iviewer('update');
         });
+
     },
     refresh: function(){
     
     },
     setMarker: function(coords){
-        var pointer = $('.wrapper').append("<div id='pointer'></div>");
+        ClickImage.pointer = $("<div id='pointer'></div>");
+        $('.wrapper').append(ClickImage.pointer);
         console.log(coords);
-        pointer.css('display', 'block');
-        pointer.css('left', coords.x+'px');
-        pointer.css('top', coords.y+'px');
+        var offset = ClickImage.viewer.iviewer('imageToContainer', coords.x, coords.y);
+        var containerOffset = ClickImage.viewer.iviewer('getContainerOffset');
+        
+        offset.x += containerOffset.left - 20;
+        offset.y += containerOffset.top - 40;
+        ClickImage.pointer.css('display', 'block');
+        ClickImage.pointer.css('left', offset.x+'px');
+        ClickImage.pointer.css('top', offset.y+'px');
     }
 };
