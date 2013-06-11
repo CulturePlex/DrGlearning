31a8c49082869ec284fa42b7d243050b094fc2d4
pedro <pedro@pedro-SATELLITE-L735.(none)>
2013-06-11 13:47:30 +0200

    Removed not necessary buttons and setted at same line as confirm button

--- gecko/www/app/js/activities/ClickImage.js
+++ gecko/www/app/js/activities/ClickImage.js
@@ -14,8 +14,9 @@ var ClickImage = {
         ClickImage.is_dragging = false;     
   
         ClickImage.viewer = $("#clickImageImage").iviewer({
-            src:"http://i618.photobucket.com/albums/tt262/royallaser/FrankZappaWereOnlyInItForTheMoneyIn.jpg", 
+            src:"http://www.eltiempo.com/blogs/caja_de_resonancia/zappamothers.jpg", 
             zoom_base:100,
+            ui_disabled:true,
             zoom_animation:false,
             onClick: function(ev, coords) {
                console.log(ClickImage.is_dragging);
@@ -39,13 +40,9 @@ var ClickImage = {
                 console.log("stop Drag");
             }
         });
-        $("#in").click(function(){ viewer.iviewer('zoom_by', 1); }); 
-        $("#out").click(function(){ viewer.iviewer('zoom_by', -1); }); 
-        $("#fit").click(function(){ viewer.iviewer('fit'); }); 
-        $("#orig").click(function(){ viewer.iviewer('set_zoom', 100); }); 
-        $("#update").click(function(){ viewer.iviewer('update_container_info'); });
-        $(".iviewer_rotate_right").hide();
-        $(".iviewer_rotate_left").hide();
+        $("#in").click(function(){ ClickImage.viewer.iviewer('zoom_by', 1); }); 
+        $("#out").click(function(){ ClickImage.viewer.iviewer('zoom_by', -1); }); 
+        $("#fit").click(function(){ ClickImage.viewer.iviewer('fit'); }); 
         $('#clickImage').live( 'pageshow',function(event){
            ClickImage.viewer.iviewer('update');
         });
